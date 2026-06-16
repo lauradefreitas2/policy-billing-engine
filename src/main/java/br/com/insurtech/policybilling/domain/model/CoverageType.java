b06ca691a8d922de94f4f46348bddbd9e07e531f
Laura de Freitas <lauradovalegameplays@gmail.com>
2026-06-16 07:19:31 -0300

    feat: implement domain layer and unit tests for Policy and MobileDevice

---
+++ src/main/java/br/com/insurtech/policybilling/domain/model/CoverageType.java
@@ -0,0 +1,5 @@
+package br.com.insurtech.policybilling.domain.model;
+
+public enum CoverageType {
+    NEW_DEVICE_REPLACEMENT
+}
---
+++ src/main/java/br/com/insurtech/policybilling/domain/model/PolicyStatus.java
@@ -0,0 +1,7 @@
+package br.com.insurtech.policybilling.domain.model;
+
+public enum PolicyStatus {
+    ACTIVE,
+    PENDING_PAYMENT,
+    CANCELED
+}
