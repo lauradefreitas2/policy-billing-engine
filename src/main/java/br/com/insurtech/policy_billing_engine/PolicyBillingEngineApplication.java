48a35812f05ce538f4018bc0f3f0d35e0aca1a81
Laura de Freitas <lauradovalegameplays@gmail.com>
2026-06-16 06:12:32 -0300

    Initial commit

---
+++ src/main/java/br/com/insurtech/policy_billing_engine/PolicyBillingEngineApplication.java
@@ -0,0 +1,13 @@
+package br.com.insurtech.policy_billing_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicyBillingEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicyBillingEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/insurtech/policy_billing_engine/PolicyBillingEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.insurtech.policy_billing_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicyBillingEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
